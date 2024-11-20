a912d09899c013daac6cc90dc312ecc04aa6d600
toxikdnb <toxikdnb@hotmail.com>
2024-11-20 17:34:37 +0000

    Added specifics class to represent specifics about the gameboy

---
+++ src/main/java/com/blackaby/Backend/Emulation/Misc/Specifics.java
@@ -0,0 +1,10 @@
+package com.blackaby.Backend.Emulation.Misc;
+
+/**
+ * This class contains specific values for the GameBoy system.
+ * These values are used by other classes to maintain consistency.
+ */
+public class Specifics {
+    public static final int GB_DISPLAY_WIDTH = 160;
+    public static final int GB_DISPLAY_HEIGHT = 144;
+}
