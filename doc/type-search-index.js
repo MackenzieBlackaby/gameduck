4d0880e1dddeb183f3b5fd36b56564aff6a3b758
toxikdnb <toxikdnb@hotmail.com>
2025-04-05 00:29:27 +0100

    Created new javadocs

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.blackaby.Frontend","l":"DuckWindow"},{"p":"com.blackaby","l":"Main"},{"p":"com.blackaby.Frontend","l":"MainWindow"},{"p":"com.blackaby.Frontend","l":"Styling"}];updateSearchResults();
