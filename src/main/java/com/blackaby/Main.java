589fe616d39b04e8780fd3125196f9309835951c
toxikdnb <toxikdnb@hotmail.com>
2025-04-04 21:44:32 +0100

    Updated look and feel of window

--- src/main/java/com/blackaby/Main.java
+++ src/main/java/com/blackaby/Main.java
@@ -1,6 +1,9 @@
 package com.blackaby;
 
 import com.blackaby.Frontend.MainWindow;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+import java.awt.Color;
 
 /**
  * This class is the main class for the GameDuck application.
@@ -8,6 +11,12 @@ import com.blackaby.Frontend.MainWindow;
  */
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.put("Button.focus", new ColorUIResource(new Color(0, 0, 0, 0)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         new MainWindow();
     }
 }
