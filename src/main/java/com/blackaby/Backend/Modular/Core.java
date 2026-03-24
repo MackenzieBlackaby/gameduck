58aaa7bec14a04544ded35b7b07b0cc6e4e7c47c
toxikdnb <toxikdnb@hotmail.com>
2026-03-24 14:24:51 +0000

    Added documentation for planning

--- src/main/java/com/blackaby/Backend/Modular/Core.java
+++ src/main/java/com/blackaby/Backend/Modular/Core.java
@@ -1,5 +1,14 @@
 package com.blackaby.Backend.Modular;
 
-public interface Core {
+/**
+ * This is a core that makes the front end seamlessly able to connect to the backend.
+ * 
+ * It should:
+ * - Take input from the front end
+ * - Output to the duckDisplay
+ * - Adjust the duck display to be configured to the specific emulated device's requirements
+ * - Have adjustable settings per core that are identified by 
+ */
+public abstract class Core {
     
 }
---
+++ src/main/java/com/blackaby/Backend/Modular/README.md
@@ -0,0 +1,8 @@
+# Modular system
+
+This branch is a planned feature of the emulator for it's V2 release
+I wanted to create this so that if I have ideas for how to implement this, I have somewhere to put them while I work on other features, while not affecting the main branch
+
+This will remain mostly unused for a while, as other features are more important for now
+
+Once this feature is created, contributions for cores would be especially welcome!
